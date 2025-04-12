b73b30b039e76721f12887ddad4f316311c07963
Artem Pugach <apugach1@pride.hofstra.edu>
2025-04-11 23:14:06 -0400

    Started setting up the Express server

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
